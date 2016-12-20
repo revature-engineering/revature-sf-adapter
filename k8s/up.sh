2555a0a34897e27f97c1b0756c5d2608adb9a668
Taylor Kemper <tjkemper6@gmail.com>
2016-12-20 17:47:54 -0500

    uncomment creation of ingress

--- k8s/up.sh
+++ k8s/up.sh
@@ -11,5 +11,5 @@ kubectl apply -f services/$APP.yaml --namespace $ENVIRONMENT
 kubectl apply -f deployments/$APP.yaml --namespace $ENVIRONMENT
 
 # ingress - don't need to expose
-#kubectl apply -f ingress/$APP-$ENVIRONMENT.yaml
+kubectl apply -f ingress/$APP-$ENVIRONMENT.yaml
 
